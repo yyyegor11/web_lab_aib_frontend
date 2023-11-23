684df21b2daa177d022afbd31b6ee3277d509e73
yyyegor11 <145067684+yyyegor11@users.noreply.github.com>
2023-11-23 18:42:58 +0300

    Create app.js

---
+++ labs/Lab_08_events_js/Solution/Task_1/app.js
@@ -0,0 +1,15 @@
+function changeBackgroundColorRed()
+{
+    document.body.style.background='red'
+}
+
+function changeBackgroundColorBlue()
+{
+    document.body.style.background='blue'
+}
+
+function changeBackgroundColorGreen()
+{
+    document.body.style.background='green'
+}
+
