7d8d254cec69d07cf86f70263330ab9f4e6b1c6c
yyyegor11 <145067684+yyyegor11@users.noreply.github.com>
2023-12-02 15:55:29 +0300

    Update Task2.py

--- labs/Lab_12_python_intro/Solution/Task2.py
+++ labs/Lab_12_python_intro/Solution/Task2.py
@@ -4,24 +4,23 @@ Count_numbers = int(input_text[0])
 input_text.pop(0)
 input_text = [int (x) for x in input_text]
 
-def Sequence(sum_medians, sub_sequence_length):
-    b = input_text[:sub_sequence_length]
-    b.sort()
-    print(b)
-    if len(b) % 2 == 1:
-        if len(b) == 1:
-            sum_medians += b[0]
+def Sequence(sum_medians, sub_sequence_length, func_input_text):
+    temp_sequence = func_input_text[:sub_sequence_length]
+    temp_sequence.sort()
+    print(temp_sequence)
+    if len(temp_sequence) % 2 == 1:
+        if len(temp_sequence) == 1:
+            sum_medians += temp_sequence[0]
         else:
-            sum_medians +=  b[((len(b) + 1) // 2) - 1]
-    elif len(b) % 2 == 0:
-        sum_medians += b[(len(b) // 2) - 1]
+            sum_medians +=  temp_sequence[((len(temp_sequence) + 1) // 2) - 1]
+    elif len(temp_sequence) % 2 == 0:
+        sum_medians += temp_sequence[(len(temp_sequence) // 2) - 1]
     print(sum_medians)
     sub_sequence_length += 1
     if sub_sequence_length > Count_numbers:
         return sum_medians
     else:
-        return Sequence(sum_medians, sub_sequence_length)
+        return Sequence(sum_medians, sub_sequence_length, func_input_text)
 
 output_file = open('output.txt', 'w')
-output_file.write(str(Sequence(0,1)))
-
+output_file.write(str(Sequence(0, 1, input_text)))
