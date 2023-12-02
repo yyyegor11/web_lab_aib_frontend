fe7b08bf9673860a7f93e8b01d2b4f32decc6fd8
yyyegor11 <145067684+yyyegor11@users.noreply.github.com>
2023-12-02 15:15:05 +0300

    Update Task2.py

--- labs/Lab_12_python_intro/Solution/Task2.py
+++ labs/Lab_12_python_intro/Solution/Task2.py
@@ -2,18 +2,12 @@ input_file = open('input.txt')
 input_text = input_file.read().split()
 Count_numbers = int(input_text[0])
 input_text.pop(0)
-if input_text[0] == 'descending':
-    input_text.pop(0)
-    input_text = [int (x) for x in input_text]
-    input_text.sort(reverse=True)
-elif input_text[0] == 'ascending':
-    input_text.pop(0)
-    input_text = [int (x) for x in input_text]
-    input_text.sort()
+input_text = [int (x) for x in input_text]
 
 def Sequence(sum_medians, sub_sequence_length):
     b = input_text[:sub_sequence_length]
     b.sort()
+    print(b)
     if len(b) % 2 == 1:
         if len(b) == 1:
             sum_medians += b[0]
@@ -21,6 +15,7 @@ def Sequence(sum_medians, sub_sequence_length):
             sum_medians +=  b[((len(b) + 1) // 2) - 1]
     elif len(b) % 2 == 0:
         sum_medians += b[(len(b) // 2) - 1]
+    print(sum_medians)
     sub_sequence_length += 1
     if sub_sequence_length > Count_numbers:
         return sum_medians
@@ -29,3 +24,4 @@ def Sequence(sum_medians, sub_sequence_length):
 
 output_file = open('output.txt', 'w')
 output_file.write(str(Sequence(0,1)))
+
