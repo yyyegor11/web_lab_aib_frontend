4577e1807ae727a9f4434ef42f819a11fdf584e1
yyyegor11 <145067684+yyyegor11@users.noreply.github.com>
2023-12-02 15:55:51 +0300

    Update Task2.py

--- labs/Lab_12_python_intro/Solution/Task2.py
+++ labs/Lab_12_python_intro/Solution/Task2.py
@@ -7,7 +7,6 @@ input_text = [int (x) for x in input_text]
 def Sequence(sum_medians, sub_sequence_length, func_input_text):
     temp_sequence = func_input_text[:sub_sequence_length]
     temp_sequence.sort()
-    print(temp_sequence)
     if len(temp_sequence) % 2 == 1:
         if len(temp_sequence) == 1:
             sum_medians += temp_sequence[0]
@@ -15,7 +14,6 @@ def Sequence(sum_medians, sub_sequence_length, func_input_text):
             sum_medians +=  temp_sequence[((len(temp_sequence) + 1) // 2) - 1]
     elif len(temp_sequence) % 2 == 0:
         sum_medians += temp_sequence[(len(temp_sequence) // 2) - 1]
-    print(sum_medians)
     sub_sequence_length += 1
     if sub_sequence_length > Count_numbers:
         return sum_medians
